6587bd4c0b54cd20fc9cdcb22b21a1b5e3f9cab9
jaydendwdsd <jayden_you@bisvietnam.com>
2024-06-01 16:58:58 +0700

    Initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int damage = 10;
+    public int health = 50;
+    public int speed = 5;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChasePlayer();
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+
+        Destroy(gameObject);
+    }
+    void ChasePlayer(){
+        transform.position = Vector3.MoveTowards(transform.position,player.transform.position,speed*Time.deltaTime);
+    
+    }
+}
